3763022a2f2d874e2108e1e98bcb2c27cbd1270e
HyunJ1017 <hyunj1017@naver.com>
2024-08-21 20:33:04 +0900

    프로그래머스 문제풀이

--- 연습/src/pkg1/run/run2.java
+++ 연습/src/pkg1/run/run2.java
@@ -11,7 +11,19 @@ public class run2 {
 		
 //		service.solution34("123");
 		
-		int i = service.solution35(null, null);
-		System.out.println(i);
+//		int i = service.solution35(null, null);
+//		System.out.println(i);
+		
+		int[] i = {};
+		i[0]=0;
+		i[1]=1;
+		i[2]=2;
+		
+		System.out.println( i[0] + "");
+		System.out.println( i[1] + "");
+		System.out.println( i[2] + "");
+		
+		
+		
 	}
 }
